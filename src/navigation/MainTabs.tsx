9935cb090f1a11d43d4ac2471d76aeecd5cc0de2
tony-97 <taaffaat_1@hotmail.com>
2025-08-07 18:46:29 -0500

    Add Material Design icons to MainTabs for improved navigation UI

--- src/navigation/MainTabs.tsx
+++ src/navigation/MainTabs.tsx
@@ -3,8 +3,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/HomeScreen';
 import FavoritesScreen from '../screens/FavoritesScreen';
 import ProfileScreen from '../screens/ProfileScreen';
-import { useSelector } from 'react-redux';
-import { RootState } from '../store/store';
+import { MaterialDesignIcons } from '@react-native-vector-icons/material-design-icons';
 
 export type MainTabParamList = {
   Home: undefined;
@@ -17,9 +16,33 @@ const Tab = createBottomTabNavigator<MainTabParamList>();
 const MainTabs = () => {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Favorites" component={FavoritesScreen} />
-      <Tab.Screen name="Profile" component={ProfileScreen} />
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialDesignIcons name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Favorites"
+        component={FavoritesScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialDesignIcons name="star" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialDesignIcons name="account" color={color} size={size} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
