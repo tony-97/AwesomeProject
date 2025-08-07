a124f97e10559a5e2a3dbd7fa9248f4da9ff82e4
tony-97 <taaffaat_1@hotmail.com>
2025-08-07 17:57:01 -0500

    Refactor FavoritesScreen to use inline rendering for favorite items; add navigation and remove favorite functionality

--- src/screens/FavoritesScreen.tsx
+++ src/screens/FavoritesScreen.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
-
-import { View, FlatList } from 'react-native';
-
+import { View, FlatList, Image, Text, Button } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
-
 import { RootState } from '../store/store';
-
 import { removeFavorite } from '../store/actions';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { RootStackParamList } from '../navigation/AppNavigator';
 
-import { PokemonItem } from '../components/PokemonItem';
+type Props = {
+  navigation: StackNavigationProp<RootStackParamList, 'Favorites'>;
+};
 
-const FavoritesScreen: React.FC = () => {
+const FavoritesScreen: React.FC<Props> = ({ navigation }) => {
   const favorites = useSelector((state: RootState) => state.favorites);
-
   const dispatch = useDispatch();
 
   return (
@@ -21,12 +20,31 @@ const FavoritesScreen: React.FC = () => {
         data={favorites}
         keyExtractor={item => item.name}
         renderItem={({ item }) => (
-          <PokemonItem
-            pokemon={item}
-            isFavorite
-            onAdd={() => {}}
-            onRemove={() => dispatch(removeFavorite(item.name))}
-          />
+          <View
+            style={{ flexDirection: 'row', alignItems: 'center', padding: 8 }}
+          >
+            <Image
+              source={{ uri: item.sprites?.front_default }}
+              style={{ width: 60, height: 60, marginRight: 12 }}
+            />
+            <View style={{ flex: 1 }}>
+              <Text style={{ fontSize: 18, textTransform: 'capitalize' }}>
+                {item.name}
+              </Text>
+              <Text style={{ color: '#555' }}>
+                {item.types.map((t: any) => t.type.name).join(', ')}
+              </Text>
+            </View>
+            <Button
+              title="Ver Detalle"
+              onPress={() => navigation.navigate('Detail', { pokemon: item })}
+            />
+            <Button
+              title="Quitar"
+              onPress={() => dispatch(removeFavorite(item.name))}
+              color="#d00"
+            />
+          </View>
         )}
       />
     </View>
